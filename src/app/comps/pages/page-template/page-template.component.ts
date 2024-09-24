b62fdacd4dfaad06fbfb3ed3d41fed1962dc2439
Vinayak Hegde <vinayak.hegde@epssw.com>
2024-09-24 18:18:20 +0530

    Created A page template

---
+++ src/app/comps/pages/page-template/page-template.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-template',
+  standalone: true,
+  imports: [],
+  templateUrl: './page-template.component.html',
+  styleUrl: './page-template.component.scss'
+})
+export class PageTemplateComponent {
+
+}
